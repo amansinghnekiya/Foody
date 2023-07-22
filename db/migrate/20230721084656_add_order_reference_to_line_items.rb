d73e8d4f90bdbed0ee1ae66b75a8f3c81211b273
Aman Singh Nekiya <amansinghnekiya@gmail.com>
2023-07-22 22:36:16 +0530

    Login Signup Front-end

--- db/migrate/20230721084656_add_order_reference_to_line_items.rb
+++
@@ -1,5 +0,0 @@
-class AddOrderReferenceToLineItems < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :line_items, :order, null: false, foreign_key: true
-  end
-end
--- db/migrate/20230721090029_drop_table_line_items.rb
+++
@@ -1,9 +0,0 @@
-class DropTableLineItems < ActiveRecord::Migration[7.0]
-  def up
-    drop_table :line_items
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end

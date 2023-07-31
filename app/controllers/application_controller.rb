3238714d55669e238e41aaf72b0fd5d0ca8cffd0
Aman Singh Nekiya <amansinghnekiya@gmail.com>
2023-07-31 17:30:50 +0530

    Set layout after active admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
-    include CurrentCart
-    before_action :set_cart
+  protect_from_forgery with: :exception
+  include CurrentCart
+  before_action :set_cart
+
+  layout :set_layout
+
+  private
+   def set_layout
+    if user_signed_in? && current_user&.role == "basic_account" || current_user&.role == "bussiness_account"
+      "application"
+    elsif !user_signed_in?
+      "application"
+    end
+   end
 end
